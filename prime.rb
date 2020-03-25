2c1fb2c8638d1432fe707f5f945efe4291c73e0e
Samiylo Kryshu <skryshu@gmail.com>
2020-03-25 18:45:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 
 def prime?(int)
-  if int < 0
+  if int > 0
     result = (1..int).select do |n|
       int % n == 0
     end
     result.length > 2 ? false : true
   end
-else
-  return false
+  else
+    return false
 end
